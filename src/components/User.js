8c232b05b01ab8de8d8e2145ec679b6c10095814
Matéo Mévollon <m.mevollon@protonmail.com>
2022-01-10 17:52:44 +0100

    fix

--- src/components/User.js
+++ src/components/User.js
@@ -11,16 +11,6 @@ function User(props) {
       <div className="grid-element">{props.user.expiration}</div>
     </React.Fragment>
   );
-  return Object.entries(props.user).map(([key, value]) => (
-    <div className="grid-element">{value}</div>
-  ));
-  // return (
-  //     {user.entries().map((attribute) => (
-  //       <div className="grid-element" key={user.firstname}>
-  //         {attribute}
-  //       </div>
-  //     ))}
-  // )
 }
 
 export default User;

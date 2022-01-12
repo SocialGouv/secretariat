6c39718c3080710b4b1f6a26603f8a2e201cb275
Matéo Mévollon <m.mevollon@protonmail.com>
2022-01-12 16:47:57 +0100

    setup

--- backend/api/app.js
+++ backend/api/app.js
@@ -1,4 +1,5 @@
-const createError = require('http-errors')
+require('dotenv').config()
+
 const express = require('express')
 const logger = require('morgan')
 
@@ -11,21 +12,5 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.use('/', indexRouter)
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404))
-})
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message
-  res.locals.error = req.app.get('env') === 'development' ? err : {}
-
-  // render the error page
-  res.status(err.status || 500)
-  res.render('error')
-})
 
 module.exports = app

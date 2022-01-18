b01138b85600a14d40e3f21308c08e9fa4fc75a4
Matéo Mévollon <m.mevollon@protonmail.com>
2022-01-18 17:39:20 +0100

    /healthz route

--- packages/backend/routes/index.js
+++ packages/backend/routes/index.js
@@ -5,4 +5,8 @@ router.get('/', function (_, res, next) {
   res.status(200).send({ text: 'up and running!' })
 })
 
+router.get('/healthz', function (_, res, next) {
+  res.status(200)
+})
+
 module.exports = router

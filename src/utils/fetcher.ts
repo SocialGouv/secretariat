15eccb6e188ab405f988aaad8d27593603e415ed
Matéo Mévollon <m.mevollon@protonmail.com>
2022-01-19 14:51:22 +0100

    unused port
    test hasura
    
    .gh
    
    fix
    
    no migrations
    
    env
    
    env var
    
    replace test
    
    apollo url
    
    resources
    
    delay
    
    hasura init
    
    hasura migration
    
    url in env
    
    table in migs
    
    copy migs and metadata
    
    startup probe
    
    no copy
    
    set probes
    
    initialDelaySeconds back
    
    Revert "initialDelaySeconds back"
    
    This reverts commit 9e22fa5d410e42206faa8492c69ecf8ab6a3f59c.
    
    more delay
    
    changed probe port
    
    port http
    
    right ports
    
    delay
    
    migrations
    
    test migs
    
    users table
    
    tables
    
    relationships
    
    table name user_team
    
    test env
    
    dev compose
    
    working register_github_user
    
    githubStatus backend
    
    github status
    
    layout ok
    
    margin
    
    conditional service status rendering
    
    catch github error
    
    deleteGithubUser
    
    search param for active user
    
    self aware nav
    
    do not focus on delete
    
    sealed-secret
    
    4xx is not an error for github API
    
    catch pseudo error
    
    no axios error
    
    deleted migs
    
    remove sealed-secret for now
    
    fix: deploy test
    
    fix: autodevops
    
    fix: json
    
    fix: favicon
    
    fix: image name
    
    fix: project name
    
    fix: healthz
    
    fix: HASURA_SECRET
    
    fix: Dockerfile
    
    feat: GH webhook
    
    fix: Clean gitignore
    
    fix: GH logs
    
    feat: GH remote schema
    
    clean DB
    
    fix: Add prettier
    
    fix: clean
    
    fix: Add github users list
    
    fix: Fetch fake github data
    
    fix: hasura migs
    
    fix: data in mig
    
    feat: insert github data in DB
    
    fix: Add views for all services
    
    fix: Use services data from hasura
    
    fix: Disable page generation on build
    
    fix: logic in utils
    
    fix: Use emails to list users
    
    fix: moved github logic
    
    feat: matomo data
    
    feat: sentry data
    
    fix: restart hasura container
    
    fix: ignore .env.local file
    
    feat: use API class
    
    feat: OVH fetching
    
    fix: zammad data
    
    feat: jwt
    
    fix: Add auth
    
    fix: broken jwt
    
    fix: Add user login to session
    
    fix: Load data after login
    
    fix: Use node:16.13-alpine
    
    fix: Test kosko-charts fix
    
    fix: Remove kosko-charts test version
    
    fix: Add app sealed secrets
    
    fix: hasura env
    
    fix: hasura_url
    
    fix: quotes
    
    fix: new sealed-secrets
    
    fix: secrets
    
    fix: branch slug
    
    fix: preprod workflow
    
    fix: add preprod env vars
    
    fix: unused echos
    
    feat: backend auth to hasura
    
    fix: hasura remote schema access
    
    fix: admin secret
    
    fix: add token to fetch sentry and matomo
    
    fix: display github data
    
    fix: fetcher instead of request
    
    fix: log token
    
    fix: better check_env
    
    fix: no classes
    
    fix: cleaner github fetcher

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -3,14 +3,10 @@ import { GraphQLClient } from "graphql-request"
 const url = process.env.NEXT_PUBLIC_HASURA_URL as string
 
 const fetcher = (query: string, token?: string, params = {}) => {
-  console.log("fetcher:", query)
-
   const client = new GraphQLClient(url)
-
   if (token && token.length) {
     client.setHeader("authorization", `Bearer ${token}`)
   }
-
   return client.request(query, params)
 }
 

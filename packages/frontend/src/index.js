8330327b10c8733f2986254a61135a03749f7ba0
Matéo Mévollon <m.mevollon@protonmail.com>
2022-01-19 15:47:17 +0100

    env var

--- packages/frontend/src/index.js
+++ packages/frontend/src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals'
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 import { BrowserRouter } from 'react-router-dom'
 
-console.log(process.env.HASURA_API_URL)
+// %%HASURA_API_URL%%
 
 const client = new ApolloClient({
   uri: 'http://localhost:8080/v1/graphql',

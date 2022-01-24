85e9f9dac8b9b37c2a80cb27523462340789e7ed
Matéo Mévollon <m.mevollon@protonmail.com>
2022-01-24 15:26:32 +0100

    githubStatus backend

--- packages/backend/routes/github.js
+++ packages/backend/routes/github.js
@@ -2,10 +2,10 @@ const express = require('express')
 const router = express.Router()
 const axios = require('axios')
 
-async function registerUser (username) {
+async function registerUser (githubUsername) {
   try {
     const getUserIdResponse = await axios({
-      url: `https://api.github.com/users/${username}`,
+      url: `https://api.github.com/users/${githubUsername}`,
       method: 'get'
     })
     await axios({
@@ -28,8 +28,36 @@ async function registerUser (username) {
   }
 }
 
+async function getUserStatus (githubUsername) {
+  try {
+    const getUserStatusResponse = await axios({
+      url: `https://api.github.com/orgs/SocialGouv/memberships/${githubUsername}`,
+      method: 'get',
+      headers: { Authorization: 'token ' + process.env.GITHUB_TOKEN }
+    })
+    console.log("Got user's status successfully")
+    console.log(getUserStatusResponse)
+    return {
+      status: 200,
+      message: getUserStatusResponse.data.state
+    }
+  } catch (error) {
+    console.error(error)
+    return {
+      status: error.response.status,
+      message: error.response.data.message
+    }
+  }
+}
+
 router.post('/register-user', function (expressRequest, expressResponse, next) {
   registerUser(expressRequest.body.input.githubUsername).then((result) => {
+    expressResponse.status(result.status).json(result)
+  })
+})
+
+router.get('/user-status', function (expressRequest, expressResponse, next) {
+  getUserStatus(expressRequest.query.username).then((result) => {
     expressResponse.status(result.status).json(result)
   })
 })

7f576403c7f956302811c78e995b4a4d20d58577
Matéo Mévollon <m.mevollon@protonmail.com>
2022-01-26 15:39:54 +0100

    fix: GH logs

--- src/pages/api/webhook.ts
+++ src/pages/api/webhook.ts
@@ -1,6 +1,23 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import crypto from 'crypto'
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-  console.log('Data from GH', req)
-  res.status(200).json({ name: 'John Doe' })
+  console.log('Headers from GH', req.headers)
+  console.log('Data from GH', req.body)
+  res.status(200).send('OK')
+}
+
+const isGithub = (req: NextApiRequest) => {
+  const payload = JSON.stringify(req.body)
+  const sig = req.headers["x-hub-signature"] || ""
+  const hmac = crypto.createHmac("sha1", process.env.GITHUB_WEBHOOK_SECRET ?? "")
+  const digest = Buffer.from(
+    "sha1=" + hmac.update(payload).digest("hex"),
+    "utf8"
+  )
+  const checksum = Buffer.from(String(sig), "utf8")
+  return !(
+    checksum.length !== digest.length ||
+    !crypto.timingSafeEqual(digest, checksum)
+  )
 }

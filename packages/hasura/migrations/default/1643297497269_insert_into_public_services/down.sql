17c4ed95eace49a6eab01c66996edb8646a6bb09
Matéo Mévollon <m.mevollon@protonmail.com>
2022-01-27 16:59:27 +0100

    fix: data in mig

--- packages/hasura/migrations/default/1643297497269_insert_into_public_services/down.sql
+++
@@ -1,1 +0,0 @@
-DELETE FROM "public"."services" WHERE "id" = '5d522461-10f9-40db-8021-7fc258e59383';
---
+++ packages/hasura/migrations/default/1643299117194_insert_into_public_services/down.sql
@@ -0,0 +1,1 @@
+DELETE FROM "public"."services" WHERE "id" = '48be6ece-17b7-46f3-af8c-993d9166374b';

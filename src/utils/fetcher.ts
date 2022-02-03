522f54a089a8b6d791e70a1589cc91f07eccefe7
Matéo Mévollon <m.mevollon@protonmail.com>
2022-02-03 15:24:44 +0100

    fix: unused files

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -3,8 +3,6 @@ import { GraphQLClient } from "graphql-request"
 const url = process.env.NEXT_PUBLIC_HASURA_URL as string
 
 const fetcher = (query: string, token?: string, params = {}) => {
-  console.log("fetcher:", query)
-
   const client = new GraphQLClient(url)
 
   if (token && token.length) {

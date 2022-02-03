bb03568097aa810c23dcc8e2f2ab883baee6d88a
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2022-02-03 11:38:11 +0100

    feat(design): Add tailwindcss (#26)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,32 @@
-import Auth from "@/components/auth"
+import { useSession } from "next-auth/react"
+
+import Login from "@/components/login"
 import { GithubUsersLoader } from "@/services/github"
 import { MatomoUsersLoader } from "@/services/matomo"
 import { SentryUsersLoader } from "@/services/sentry"
 
 const Page = () => {
+  const { data: session } = useSession()
+
+  if (!session) {
+    return (
+      <div className="container">
+        <main>
+          <Login />
+        </main>
+      </div>
+    )
+  }
+
   return (
     <div className="container">
-      <Auth />
       <main>
         <h2>Github</h2>
         <GithubUsersLoader />
+        <br />
         <h2>Matomo</h2>
         <MatomoUsersLoader />
+        <br />
         <h2>Sentry</h2>
         <SentryUsersLoader />
       </main>

b4510826165b3df7a95805d9ea985193d09a9083
gary-van-woerkens <gary.van-woerkens@sg.social.gouv.fr>
2022-02-08 15:07:34 +0100

    fix: OVH users list

--- src/components/ovh-users.tsx
+++ src/components/ovh-users.tsx
@@ -1,16 +1,56 @@
-import { useRouter } from "next/router"
+import Image from "next/image"
+import { useState } from "react"
 
 const Users = ({ users = [] }: { users: OVHUser[] }) => {
-  const { query } = useRouter()
+  const [selectedUser, setSelectedUser] = useState<OVHUser>()
 
   return (
-    <ul className="users">
-      {users.map(({ email }, i) => (
-        <li key={i}>
-          <a>{email}</a>
-        </li>
-      ))}
-    </ul>
+    <div className="github-users">
+      <ul className="user-list">
+        {users.map((user, i) => (
+          <li
+            key={i}
+            className={`tile${
+              selectedUser && selectedUser.login === user.login
+                ? " selected"
+                : ""
+            }`}
+            onClick={() => setSelectedUser(user)}
+          >
+            <div className="user">
+              <Image
+                width={48}
+                height={48}
+                alt="user avatar"
+                src="/images/avatar.jpeg"
+              />
+              <div className="info">
+                <h3>
+                  {user.firstName
+                    ? `${user.firstName} ${user.lastName}`
+                    : user.displayName}{" "}
+                  <span>({user.login})</span>
+                </h3>
+                <div className="email">{user.primaryEmailAddress}</div>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+      {selectedUser && (
+        <div className="selected-user">
+          <div className="sticky-container">
+            <div className="user-profile">
+              <div>
+                Nom: {selectedUser.firstName} {selectedUser.lastName}
+              </div>
+              <div>Login: {selectedUser.login}</div>
+              <div>Email: {selectedUser.primaryEmailAddress}</div>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
   )
 }
 

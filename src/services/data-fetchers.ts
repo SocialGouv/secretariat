8b1025b8697fc9a8c7cfa1b1d2c39745d6a285b1
Matéo Mévollon <38255502+Matmut7@users.noreply.github.com>
2022-02-08 17:40:19 +0100

    refactor: delays using p-map (#46)

--- src/services/data-fetchers.ts
+++ src/services/data-fetchers.ts
@@ -1,14 +1,43 @@
+import { getRemoteGithubTeams, getRemoteGithubUsers } from "@/queries/index"
 import fetcher from "@/utils/fetcher"
 import { getJwt } from "@/utils/jwt"
-import { checkEnv } from "@/utils/services-fetching"
-import { FetchedData } from "@/utils/services-fetching"
-import { getRemoteGithubUsers, getRemoteGithubTeams } from "@/queries/index"
+import { gql } from "graphql-request"
+import pMap from "p-map"
+import { setTimeout } from "timers/promises"
 
 const DEFAULT_DELAY = 100
 
-// Used to `await delay(ms)` before queries we want to delay
-const delay = (ms: number) => {
-  return new Promise((resolve) => setTimeout(resolve, ms))
+export type FetchedData = Record<string, unknown> | Record<string, unknown>[]
+
+// Each service fetcher requires some credentials or a token to work
+export const checkEnv = (envVars: string[]) => {
+  envVars.forEach((envVar) => {
+    if (!process.env[envVar]) {
+      throw ReferenceError(`Could not find ${envVar} environment variable`)
+    }
+  })
+}
+
+export const updateDbWithData = (
+  serviceName: string,
+  data: FetchedData,
+  jwt: string
+) => {
+  checkEnv(["NEXT_PUBLIC_HASURA_URL"])
+
+  fetcher(
+    gql`
+      mutation UpdateData($data: jsonb!) {
+        update_services(where: {}, _set: { ${serviceName}: $data }) {
+          returning {
+            id
+          }
+        }
+      }
+`,
+    jwt,
+    { data }
+  )
 }
 
 // We have too many users to receive them all in the first page
@@ -36,7 +65,7 @@ export const github = async (msDelay = DEFAULT_DELAY): Promise<FetchedData> => {
   users.push(...usersPage)
 
   while (hasNextPage) {
-    await delay(msDelay) // so that we don't spam the remote API
+    await setTimeout(msDelay) // so that we don't spam the remote API
     ;({ usersPage, hasNextPage, endCursor } = await fetchGithubPage(
       jwt,
       endCursor
@@ -45,17 +74,19 @@ export const github = async (msDelay = DEFAULT_DELAY): Promise<FetchedData> => {
   }
 
   // Update each user with its teams
-  const usersWithTeams = Promise.all(
-    users.map(async (user, index) => {
-      await delay(index * msDelay)
+  const usersWithTeams = pMap(
+    users,
+    async (user, index) => {
       const {
         organization: {
           teams: { nodes: teamsList },
         },
       } = await fetcher(getRemoteGithubTeams, jwt, { userLogins: user.login })
       console.log(`fetched teams for Github user ${index + 1}/${users.length}`)
+      await setTimeout(msDelay) // so that we don't spam the remote API
       return { ...user, teams: teamsList }
-    })
+    },
+    { concurrency: 1 }
   )
   return usersWithTeams
 }
@@ -129,21 +160,20 @@ export const nextcloud = async (
     ocs: {
       data: { users: logins },
     },
-  }: {
-    ocs: {
-      data: { users: string[] }
-    }
   } = await response.json()
-  const users = await Promise.all(
-    logins.map(async (login: string, index: number) => {
-      await delay(index * msDelay)
+  const users = pMap(
+    logins,
+    async (login: string, index: number) => {
       const {
         ocs: { data: user },
       } = await fetchNextcloudUser(login)
       console.log(`fetched Nextcloud user ${index + 1}/${logins.length}`)
+      await setTimeout(msDelay)
       return user
-    })
+    },
+    { concurrency: 1 }
   )
+
   return users
 }
 
@@ -175,13 +205,15 @@ export const ovh = async (msDelay = DEFAULT_DELAY): Promise<FetchedData> => {
   )
 
   // OVH only sends us a list of emails, we need to query each user's details
-  const users: Record<string, unknown>[] = await Promise.all(
-    emails.map(async (email: string, index: number) => {
-      await delay(index * msDelay)
+  const users = pMap(
+    emails,
+    async (email: string, index: number) => {
       const user = await fetchOvhUser(ovh, email)
       console.log(`fetched OVH user ${index + 1}/${emails.length}`)
+      await setTimeout(msDelay)
       return user
-    })
+    },
+    { concurrency: 1 }
   )
   return users
 }
@@ -212,4 +244,31 @@ export const zammad = async (): Promise<FetchedData> => {
     }
   )
   return response.json()
+}
+
+const SERVICES = [
+  "github",
+  "matomo",
+  "mattermost",
+  "nextcloud",
+  "ovh",
+  "sentry",
+  "zammad",
+] as const
+
+const servicesFetchers = {
+  github,
+  matomo,
+  mattermost,
+  nextcloud,
+  ovh,
+  sentry,
+  zammad,
+}
+
+export const fetchAndUpdateServices = (jwt: string) => {
+  SERVICES.forEach(async (serviceName) => {
+    const data = await servicesFetchers[serviceName]()
+    updateDbWithData(serviceName, data, jwt)
+  })
 }

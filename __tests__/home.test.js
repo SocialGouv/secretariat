10cc6d108cff666e91ea55c8cf1e490287f02002
gary-van-woerkens <gary.van-woerkens@sg.social.gouv.fr>
2022-02-11 18:34:28 +0100

    fix: Add code coverage

--- __tests__/home.test.js
+++ __tests__/home.test.js
@@ -4,6 +4,7 @@ import { render } from "@testing-library/react"
 import Home from "../src/pages/index"
 import { useSession } from "next-auth/react"
 
+// https://github.com/nextauthjs/next-auth/issues/775
 jest.mock("next-auth/react")
 
 it("renders homepage", () => {

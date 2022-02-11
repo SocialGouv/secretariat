3cb10fcaf5cb5bc38f355c9d776fa5c9f59e8038
Matéo Mévollon <m.mevollon@protonmail.com>
2022-02-11 18:07:52 +0100

    fix: mattermost paging

--- src/services/fetchers/mattermost.ts
+++ src/services/fetchers/mattermost.ts
@@ -1,9 +1,14 @@
 import { MATTERMOST_API_TOKEN } from "@/utils/env"
-import { FetchedData } from "../fetch"
+import { setTimeout } from "timers/promises"
+import { DEFAULT_DELAY } from "../fetch"
 
-export const fetchMattermostUsers = async (): Promise<FetchedData> => {
+export const fetchMattermostUsers = async (
+  users: Record<string, unknown>[] = [],
+  page: number = 0,
+  msDelay = DEFAULT_DELAY
+): Promise<Record<string, unknown>[]> => {
   const response = await fetch(
-    "https://mattermost.fabrique.social.gouv.fr/api/v4/users",
+    `https://mattermost.fabrique.social.gouv.fr/api/v4/users?page=${page}&per_page=200&active=true`,
     {
       method: "GET",
       headers: {
@@ -11,5 +16,11 @@ export const fetchMattermostUsers = async (): Promise<FetchedData> => {
       },
     }
   )
-  return response.json()
+  const usersPage: Record<string, unknown>[] = await response.json()
+  if (usersPage.length === 0) {
+    return users
+  } else {
+    await setTimeout(msDelay) // so that we don't spam the remote API
+    return [...users, ...(await fetchMattermostUsers(users, page + 1))]
+  }
 }

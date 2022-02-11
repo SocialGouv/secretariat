4533a6e6182f35f90dd6d67e6d6acb317653306c
gary-van-woerkens <gary.van-woerkens@sg.social.gouv.fr>
2022-02-11 13:59:35 +0100

    fix: Floating user profile

--- src/components/github-users.tsx
+++ src/components/github-users.tsx
@@ -1,9 +1,110 @@
+import Image from "next/image"
 import { useState } from "react"
 
 import Users from "@/components/users/index"
 import useGithubUsers from "@/services/github"
 import UserList from "@/components/users/user-list"
 import UserProfile from "@/components/users/user-profile"
+
+const GithubUserProfile = ({ user }: { user: GithubUser }) => (
+  <UserProfile>
+    <div className="flex items-center mb-4">
+      <Image width={48} height={48} alt="user avatar" src={user.avatarUrl} />
+      <h2 className="ml-4">{user.name}</h2>
+    </div>
+    <h3>Github</h3>
+    <hr className="my-2" />
+    <ul className="text-sm">
+      <li>
+        <strong>Login</strong>: {user.login}
+      </li>
+      <li>
+        <strong>Email</strong>: {user.email}
+      </li>
+      <li>
+        <strong>ID</strong>: {user.id}
+      </li>
+    </ul>
+    <h3 className="mt-4">Matomo</h3>
+    <hr className="my-2" />
+    <ul className="text-sm">
+      <li>
+        <strong>Login</strong>: {user.login}
+      </li>
+      <li>
+        <strong>Email</strong>: {user.email}
+      </li>
+      <li>
+        <strong>ID</strong>: {user.id}
+      </li>
+    </ul>
+    <h3 className="mt-4">Sentry</h3>
+    <hr className="my-2" />
+    <ul className="text-sm">
+      <li>
+        <strong>Login</strong>: {user.login}
+      </li>
+      <li>
+        <strong>Email</strong>: {user.email}
+      </li>
+      <li>
+        <strong>ID</strong>: {user.id}
+      </li>
+    </ul>
+    <h3 className="mt-4">OVH</h3>
+    <hr className="my-2" />
+    <ul className="text-sm">
+      <li>
+        <strong>Login</strong>: {user.login}
+      </li>
+      <li>
+        <strong>Email</strong>: {user.email}
+      </li>
+      <li>
+        <strong>ID</strong>: {user.id}
+      </li>
+    </ul>
+    <h3 className="mt-4">Pastek</h3>
+    <hr className="my-2" />
+    <ul className="text-sm">
+      <li>
+        <strong>Login</strong>: {user.login}
+      </li>
+      <li>
+        <strong>Email</strong>: {user.email}
+      </li>
+      <li>
+        <strong>ID</strong>: {user.id}
+      </li>
+    </ul>
+    <h3 className="mt-4">Mattermost</h3>
+    <hr className="my-2" />
+    <ul className="text-sm">
+      <li>
+        <strong>Login</strong>: {user.login}
+      </li>
+      <li>
+        <strong>Email</strong>: {user.email}
+      </li>
+      <li>
+        <strong>ID</strong>: {user.id}
+      </li>
+    </ul>
+    <h3 className="mt-4">NextCloud</h3>
+    <hr className="my-2" />
+    <ul className="text-sm">
+      <li>
+        <strong>Login</strong>: {user.login}
+      </li>
+      <li>
+        <strong>Email</strong>: {user.email}
+      </li>
+      <li>
+        <strong>ID</strong>: {user.id}
+      </li>
+    </ul>
+  </UserProfile>
+)
 
 const GithubUsers = () => {
   const users = useGithubUsers()
@@ -18,14 +119,7 @@ const GithubUsers = () => {
           getUserData={(user) => user as User}
           onSelect={(user) => setSelectedUser(user as GithubUser)}
         />
-        {selectedUser && (
-          <UserProfile>
-            <div>Nom: {selectedUser.name}</div>
-            <div>Login: {selectedUser.login}</div>
-            <div>Email: {selectedUser.email}</div>
-            <div>ID: {selectedUser.id}</div>
-          </UserProfile>
-        )}
+        {selectedUser && <GithubUserProfile user={selectedUser} />}
       </>
     </Users>
   )

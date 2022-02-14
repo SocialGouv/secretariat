e56af9a35d26bfe09054aa9d87a494921f9bee57
Matéo Mévollon <m.mevollon@protonmail.com>
2022-02-14 10:07:50 +0100

    fix: accumulator

--- src/services/fetchers/mattermost.ts
+++ src/services/fetchers/mattermost.ts
@@ -2,13 +2,15 @@ import { MATTERMOST_API_TOKEN } from "@/utils/env"
 import { setTimeout } from "timers/promises"
 import { DEFAULT_DELAY } from "../fetch"
 
+const PAGE_SIZE = 200 // maximum for Mattermost's API
+
 export const fetchMattermostUsers = async (
   users: Record<string, unknown>[] = [],
   page: number = 0,
   msDelay = DEFAULT_DELAY
 ): Promise<Record<string, unknown>[]> => {
   const response = await fetch(
-    `https://mattermost.fabrique.social.gouv.fr/api/v4/users?page=${page}&per_page=200&active=true`,
+    `https://mattermost.fabrique.social.gouv.fr/api/v4/users?page=${page}&per_page=${PAGE_SIZE}&active=true`,
     {
       method: "GET",
       headers: {
@@ -21,6 +23,6 @@ export const fetchMattermostUsers = async (
     return users
   } else {
     await setTimeout(msDelay) // so that we don't spam the remote API
-    return [...users, ...(await fetchMattermostUsers(users, page + 1))]
+    return fetchMattermostUsers([...users, ...usersPage], page + 1, msDelay)
   }
 }

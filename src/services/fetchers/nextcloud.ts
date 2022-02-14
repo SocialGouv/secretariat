4efe92fe2a65168ef0985b5d19847e1eb7e35963
Matéo Mévollon <38255502+Matmut7@users.noreply.github.com>
2022-02-14 17:26:01 +0100

    fix: nextcloud pagination (#66)

--- src/services/fetchers/nextcloud.ts
+++ src/services/fetchers/nextcloud.ts
@@ -3,6 +3,8 @@ import pMap from "p-map"
 import { setTimeout } from "timers/promises"
 import { DEFAULT_DELAY, FetchedData } from "../fetch"
 
+const PAGE_SIZE = 100 // not sure about the real max for Nextcloud's API
+
 const fetchNextcloudUser = async (login: string) => {
   const user = await fetch(
     `https://nextcloud.fabrique.social.gouv.fr/ocs/v1.php/cloud/users/${login}`,
@@ -21,10 +23,14 @@ const fetchNextcloudUser = async (login: string) => {
 }
 
 export const fetchNextcloudUsers = async (
+  logins: string[] = [],
+  page = 0,
   msDelay = DEFAULT_DELAY
 ): Promise<FetchedData> => {
   const response = await fetch(
-    "https://nextcloud.fabrique.social.gouv.fr/ocs/v1.php/cloud/users",
+    `https://nextcloud.fabrique.social.gouv.fr/ocs/v1.php/cloud/users?limit=${PAGE_SIZE}&offset=${
+      page * PAGE_SIZE
+    }`,
     {
       method: "GET",
       headers: {
@@ -37,24 +43,28 @@ export const fetchNextcloudUsers = async (
     }
   )
 
-  // Nextcloud only sends us a list of logins, we need to query each user's details
   const {
     ocs: {
-      data: { users: logins },
+      data: { users: loginsPage },
     },
   } = await response.json()
-  const users = pMap(
-    logins,
-    async (login: string, index: number) => {
-      const {
-        ocs: { data: user },
-      } = await fetchNextcloudUser(login)
-      console.log(`fetched Nextcloud user ${index + 1}/${logins.length}`)
-      await setTimeout(msDelay)
-      return user
-    },
-    { concurrency: 1 }
-  )
 
-  return users
+  if (loginsPage.length === 0) {
+    // Nextcloud only sends us a list of logins, we need to query each user's details
+    return pMap(
+      logins,
+      async (login: string, index: number) => {
+        const {
+          ocs: { data: user },
+        } = await fetchNextcloudUser(login)
+        console.log(`fetched Nextcloud user ${index + 1}/${logins.length}`)
+        await setTimeout(msDelay)
+        return user
+      },
+      { concurrency: 1 }
+    )
+  } else {
+    await setTimeout(msDelay) // so that we don't spam the remote API
+    return fetchNextcloudUsers([...logins, ...loginsPage], page + 1, msDelay)
+  }
 }

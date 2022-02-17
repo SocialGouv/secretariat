52dbddcb09b4a8c000fecdf1495dee95c174f57a
gary-van-woerkens <gary.van-woerkens@sg.social.gouv.fr>
2022-02-17 17:51:09 +0100

    fix app build

--- src/components/common/logo/nextcloud.tsx
+++ src/components/common/logo/nextcloud.tsx
@@ -1,11 +1,20 @@
 import Image from "next/image"
 
-const NextCloudLogo = () => (
+const ratio = 1
+
+const sizes: Record<string, Record<string, number>> = {
+  s: { width: 16 * ratio, height: 16 },
+  m: { width: 18 * ratio, height: 18 },
+  l: { width: 24 * ratio, height: 24 },
+}
+
+const NextCloudLogo = ({ size = "m" }: { size?: string }) => (
   <Image
-    width={32}
-    height={32}
+    title="nextcloud"
     alt="nextcloud logo"
+    width={sizes[size].width}
     src={"/images/nextcloud.png"}
+    height={sizes[size].height}
   />
 )
 
--- src/components/common/logo/zammad.tsx
+++ src/components/common/logo/zammad.tsx
@@ -1,7 +1,21 @@
 import Image from "next/image"
 
-const ZammadLogo = () => (
-  <Image src={"/images/zammad.png"} height={32} width={32} alt="zammad logo" />
+const ratio = 1
+
+const sizes: Record<string, Record<string, number>> = {
+  s: { width: 16 * ratio, height: 16 },
+  m: { width: 18 * ratio, height: 18 },
+  l: { width: 24 * ratio, height: 24 },
+}
+
+const ZammadLogo = ({ size = "m" }: { size?: string }) => (
+  <Image
+    title="zammad"
+    alt="zammad logo"
+    width={sizes[size].width}
+    src={"/images/zammad.png"}
+    height={sizes[size].height}
+  />
 )
 
 export default ZammadLogo

24433f129a38b33435edce363b8619ed8f01cd60
Matéo Mévollon <m.mevollon@protonmail.com>
2022-02-21 17:46:02 +0100

    add warning count in sql view

---
+++ packages/hasura/migrations/default/1645461402272_run_sql_migration/up.sql
@@ -0,0 +1,11 @@
+CREATE OR REPLACE VIEW "public"."services_count" AS 
+ SELECT jsonb_array_length(services.github) AS github,
+    jsonb_array_length(services.sentry) AS sentry,
+    jsonb_array_length(services.matomo) AS matomo,
+    jsonb_array_length(services.ovh) AS ovh,
+    jsonb_array_length(services.mattermost) AS mattermost,
+    jsonb_array_length(services.zammad) AS zammad,
+    jsonb_array_length(services.nextcloud) AS nextcloud,
+    ( SELECT count(*) AS count FROM users) AS users,
+    ( SELECT count(*) FROM users WHERE warning>0 ) as warnings
+   FROM services;

b832f92f96296b60250b8736c902e1c0f7855ed4
Matéo Mévollon <m.mevollon@protonmail.com>
2022-02-22 15:29:13 +0100

    zammad

---
+++ __tests__/services/fetchers/zammad.test.js
@@ -0,0 +1,22 @@
+import fetcher from "@/utils/rest-fetcher"
+import { fetchZammadUsers } from "@/services/fetchers/zammad"
+
+jest.mock("@/utils/rest-fetcher")
+
+it("should return a list of fetched users", async () => {
+  fetcher
+    .mockResolvedValueOnce(
+      Promise.resolve({ json: () => Promise.resolve([{}, {}, {}]) })
+    )
+    .mockResolvedValue(Promise.resolve({ json: () => Promise.resolve([]) }))
+
+  const result = await fetchZammadUsers()
+  expect(result).toMatchObject([{}, {}, {}])
+})
+
+it("should return an empty list if no response", async () => {
+  fetcher.mockResolvedValue(Promise.resolve(null))
+
+  const result = await fetchZammadUsers()
+  expect(result).toMatchObject([])
+})

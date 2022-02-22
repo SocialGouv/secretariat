1d59cca4d8ea550582931595496ee6fe4a202d92
Matéo Mévollon <m.mevollon@protonmail.com>
2022-02-22 17:36:14 +0100

    ovh

---
+++ __tests__/services/fetchers/ovh.test.js
@@ -0,0 +1,28 @@
+import { fetchOvhUsers } from "@/services/fetchers/ovh"
+
+beforeEach(() => {
+  jest.resetModules()
+})
+
+it("should return a list of fetched users", async () => {
+  jest.mock("ovh", () => () => ({
+    requestPromised: jest
+      .fn()
+      .mockResolvedValueOnce(Promise.resolve(["", "", ""]))
+      .mockResolvedValue(Promise.resolve([{}, {}, {}])),
+  }))
+
+  const result = await fetchOvhUsers()
+  expect(result).toMatchObject([{}, {}, {}])
+})
+
+it("should return an empty list on catch", async () => {
+  jest.mock("ovh", () => () => ({
+    requestPromised: () => {
+      throw Error
+    },
+  }))
+
+  const result = await fetchOvhUsers()
+  expect(result).toMatchObject([])
+})

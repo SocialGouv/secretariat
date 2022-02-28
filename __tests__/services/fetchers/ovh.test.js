3b8a15e8556a2b9638d48190ece636984f790f79
Matéo Mévollon <m.mevollon@protonmail.com>
2022-02-28 09:32:13 +0100

    fix: ovh test

--- __tests__/services/fetchers/ovh.test.js
+++ __tests__/services/fetchers/ovh.test.js
@@ -9,11 +9,11 @@ it("should return a list of fetched users", async () => {
     requestPromised: jest
       .fn()
       .mockResolvedValueOnce(Promise.resolve(["", "", ""]))
-      .mockResolvedValue(Promise.resolve([{}, {}, {}])),
+      .mockResolvedValue(Promise.resolve({})),
   }))
 
   const result = await fetchOvhUsers()
-  expect(result).toMatchObject([{}, {}, {}])
+  expect(result).toStrictEqual([{}, {}, {}])
 })
 
 it("should return an empty list on catch", async () => {

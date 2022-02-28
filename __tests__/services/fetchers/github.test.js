8a98a1b6553462cc1e6c0144fa9da05b7f8dd30b
Matéo Mévollon <m.mevollon@protonmail.com>
2022-02-28 10:23:18 +0100

    github test

---
+++ __tests__/services/fetchers/github.test.js
@@ -0,0 +1,45 @@
+import fetcher from "@/utils/fetcher"
+import { fetchGithubUsers } from "@/services/fetchers/github"
+
+jest.mock("@/utils/fetcher")
+jest.mock("@/utils/jwt", () => ({
+  getJwt: () => "",
+}))
+
+it("should return a list of fetched users", async () => {
+  fetcher
+    .mockResolvedValueOnce(
+      Promise.resolve({
+        organization: {
+          membersWithRole: {
+            nodes: [{}, {}, {}],
+            pageInfo: { hasNextPage: true, endCursor: "" },
+          },
+        },
+      })
+    )
+    .mockResolvedValueOnce(
+      Promise.resolve({
+        organization: {
+          membersWithRole: {
+            nodes: [],
+            pageInfo: { hasNextPage: false, endCursor: "" },
+          },
+        },
+      })
+    )
+    .mockResolvedValue(
+      Promise.resolve({
+        organization: {
+          teams: { nodes: ["", ""] },
+        },
+      })
+    )
+
+  const result = await fetchGithubUsers()
+  expect(result).toStrictEqual([
+    { teams: ["", ""] },
+    { teams: ["", ""] },
+    { teams: ["", ""] },
+  ])
+})

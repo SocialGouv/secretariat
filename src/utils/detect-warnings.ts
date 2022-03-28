b6f3c15e21f0a9b85e707819796b51913e421582
Matéo Mévollon <38255502+Matmut7@users.noreply.github.com>
2022-03-28 14:58:10 +0200

    Warnings (#148)
    * fix: better warning name
    
    * fix: backend's hasura permissions
    
    * feat: new warning

--- src/utils/detect-warnings.ts
+++ src/utils/detect-warnings.ts
@@ -1,4 +1,5 @@
 const noAloneServices = (userEntry: Record<string, unknown>) => {
+  // Services that should not be alone in an entry
   const FORBIDDEN_ALONE_SERVICES: ServiceName[] = [
     "sentry",
     "zammad",
@@ -11,12 +12,25 @@ const noAloneServices = (userEntry: Record<string, unknown>) => {
     (key) => key !== "id" && userEntry[key] !== null
   ) as ServiceName[]
   return services.length === 1 && FORBIDDEN_ALONE_SERVICES.includes(services[0])
-    ? "no_alone_services"
+    ? "alone_service"
     : null
 }
 
-// Insert a rule function here to active the rule
-const ACTIVE_RULES = [noAloneServices]
+const containSpecificServices = (userEntry: Record<string, unknown>) => {
+  // We want to find at least one these services in each entry
+  const MANDATORY_SERVICES: ServiceName[] = ["github", "mattermost"]
+
+  const services = Object.keys(userEntry).filter(
+    (key) => key !== "id" && userEntry[key] !== null
+  ) as ServiceName[]
+  return services.some((element) => MANDATORY_SERVICES.includes(element))
+    ? null
+    : "missing_services"
+}
+
+// Insert a rule function here to activate the rule
+// The retained warning will be the first found, from left to right
+const ACTIVE_RULES = [noAloneServices, containSpecificServices]
 
 export const detectWarnings = (
   userEntry: Record<string, Record<string, unknown>>

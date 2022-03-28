4e1cea0de4a7aba5f440418280caf176c997f617
gary-van-woerkens <gary.van-woerkens@sg.social.gouv.fr>
2022-03-28 19:19:16 +0200

    fix: change warning wording

--- src/components/users/user-warning.tsx
+++ src/components/users/user-warning.tsx
@@ -3,7 +3,7 @@ import Alert from "@/components/common/alert"
 const UserWarning = ({ type }: { type?: string }) => (
   <div className="user-warning">
     {type === "alone_service" && (
-      <Alert message="Cet utilisateur n'est constitué que d'un seul service." />
+      <Alert message="Cet utilisateur n'est attaché qu'a un seul service." />
     )}
     {type === "missing_services" && (
       <Alert message="Cet utilisateur n'a pas de compte Github ou Mattermost." />

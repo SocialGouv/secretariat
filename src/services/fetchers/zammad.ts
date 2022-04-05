d7e55a287d447319a37482b7ee1b2d6b845fccb1
Matéo Mévollon <m.mevollon@protonmail.com>
2022-04-05 18:00:50 +0200

    feat: zammad groups

--- src/services/fetchers/zammad.ts
+++ src/services/fetchers/zammad.ts
@@ -4,9 +4,22 @@ import { setTimeout } from "timers/promises"
 
 const PAGE_SIZE = 200
 
+const fetchZammadGroups = async (): Promise<{ id: number }[]> => {
+  const response = await fetcher(
+    `https://pastek.fabrique.social.gouv.fr/api/v1/groups`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${ZAMMAD_API_TOKEN}`,
+      },
+    }
+  )
+  return response?.json()
+}
+
 export const fetchZammadUsers = async (
   msDelay: number,
-  users: Record<string, unknown>[] = [],
+  users: { group_ids: Record<string, unknown> }[] = [],
   page = 1
 ): Promise<Record<string, unknown>[]> => {
   const response = await fetcher(
@@ -19,10 +32,16 @@ export const fetchZammadUsers = async (
     }
   )
   const usersPage = response ? await response.json() : []
+  await setTimeout(msDelay) // so that we don't spam the remote API
   if (usersPage.length === 0) {
-    return users
+    const allGroups = await fetchZammadGroups()
+    return users.map((user) => ({
+      ...user,
+      groups: allGroups.filter((group) =>
+        Object.keys(user.group_ids).includes(group.id.toString())
+      ),
+    }))
   } else {
-    await setTimeout(msDelay) // so that we don't spam the remote API
     return fetchZammadUsers(msDelay, [...users, ...usersPage], page + 1)
   }
 }

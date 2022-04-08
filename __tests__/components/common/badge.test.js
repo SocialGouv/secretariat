244b986c20ff34a7f3186fb5256321fee9f8bfb2
gary-van-woerkens <gary.van-woerkens@sg.social.gouv.fr>
2022-04-08 12:47:53 +0200

    chore(tests): add common components tests

---
+++ __tests__/components/common/badge.test.js
@@ -0,0 +1,24 @@
+import { render } from "@testing-library/react"
+
+import Badge from "../../../src/components/common/badge"
+
+it("renders expiry badge", () => {
+  const { container } = render(
+    <Badge type="expiry" label="expiry" title="expiry" />
+  )
+  expect(container).toMatchSnapshot()
+})
+
+it("renders warning badge", () => {
+  const { container } = render(
+    <Badge type="warning" label="warning" title="warning" />
+  )
+  expect(container).toMatchSnapshot()
+})
+
+it("renders disabled badge", () => {
+  const { container } = render(
+    <Badge type="disabled" label="disabled" title="disabled" />
+  )
+  expect(container).toMatchSnapshot()
+})
---
+++ __tests__/components/common/service-logo.test.js
@@ -0,0 +1,18 @@
+import { render } from "@testing-library/react"
+
+import ServiceLogo from "../../../src/components/common/service-logo"
+
+it("renders small service logo", () => {
+  const { container } = render(<ServiceLogo size="sm" name="matomo" />)
+  expect(container).toMatchSnapshot()
+})
+
+it("renders medium service logo", () => {
+  const { container } = render(<ServiceLogo name="matomo" />)
+  expect(container).toMatchSnapshot()
+})
+
+it("renders large service logo", () => {
+  const { container } = render(<ServiceLogo size="lg" name="matomo" />)
+  expect(container).toMatchSnapshot()
+})

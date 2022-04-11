96d42d398f8dc7cf07dda68d4309705e7d190233
Matéo Mévollon <38255502+Matmut7@users.noreply.github.com>
2022-04-11 11:34:20 +0200

    fix: fetch matomo sites (#207)

--- src/services/fetchers/matomo.ts
+++ src/services/fetchers/matomo.ts
@@ -5,16 +5,78 @@ import { setTimeout } from "timers/promises"
 
 const PAGE_SIZE = 100 // maximum for Matomo's API
 
-const formatUsers = (users: Record<string, unknown>[]) => {
-  return users.map((user) => ({
-    ...user,
-    id: randomUUID(),
-  }))
+const formatUser = (user: Record<string, unknown>) => ({
+  ...user,
+  id: randomUUID(),
+})
+
+const fetchSitesNames = async (
+  msDelay: number,
+  users: { sites: Record<string, unknown>[] }[],
+  sites: Record<string, unknown>[] = [],
+  page: number = 0
+): Promise<Record<string, unknown>[]> => {
+  await setTimeout(msDelay)
+  const response = await fetcher(
+    "https://matomo.fabrique.social.gouv.fr/index.php",
+    {
+      method: "POST",
+      headers: {
+        "content-type": "application/x-www-form-urlencoded",
+      },
+      body: `module=API&method=SitesManager.getAllSites&filter_limit=${PAGE_SIZE}&filter_offset=${
+        PAGE_SIZE * page
+      }&format=json&token_auth=${MATOMO_API_TOKEN}`,
+    }
+  )
+  const sitesPage = response ? await response.json() : []
+
+  if (sitesPage.length === 0) {
+    return users.map((user) => ({
+      ...user,
+      sites: user.sites.map((userSite) => ({
+        ...userSite,
+        ...sites.find((site) => site.idsite === userSite.site),
+      })),
+    }))
+  } else {
+    return fetchSitesNames(msDelay, users, [...sites, ...sitesPage], page + 1)
+  }
+}
+
+const fetchUserSites = async (
+  msDelay: number,
+  user: Record<string, unknown>,
+  sites: Record<string, unknown>[] = [],
+  page: number = 0
+): Promise<Record<string, unknown>[]> => {
+  await setTimeout(msDelay)
+  const response = await fetcher(
+    "https://matomo.fabrique.social.gouv.fr/index.php",
+    {
+      method: "POST",
+      headers: {
+        "content-type": "application/x-www-form-urlencoded",
+      },
+      body: `module=API&method=UsersManager.getSitesAccessFromUser&userLogin=${
+        user.login
+      }&filter_limit=${PAGE_SIZE}&filter_offset=${
+        PAGE_SIZE * page
+      }&format=json&token_auth=${MATOMO_API_TOKEN}`,
+    }
+  )
+  const sitesPage = response ? await response.json() : []
+
+  if (sitesPage.length === 0) {
+    return sites
+  } else {
+    return fetchUserSites(msDelay, user, [...sites, ...sitesPage], page + 1)
+  }
 }
 
 export const fetchMatomoUsers = async (
   msDelay: number,
-  users: Record<string, unknown>[] = [],
+  users: { sites: Record<string, unknown>[] }[] = [],
   page: number = 0
 ): Promise<Record<string, unknown>[]> => {
   const response = await fetcher(
@@ -31,7 +93,13 @@ export const fetchMatomoUsers = async (
   )
   const usersPage = response ? await response.json() : []
   if (usersPage.length === 0) {
-    return formatUsers(users)
+    const usersWithSites = await Promise.all(
+      users.map(async (user) => ({
+        ...formatUser(user),
+        sites: await fetchUserSites(msDelay, user),
+      }))
+    )
+    return fetchSitesNames(msDelay, usersWithSites)
   } else {
     await setTimeout(msDelay) // so that we don't spam the remote API
     return fetchMatomoUsers(msDelay, [...users, ...usersPage], page + 1)

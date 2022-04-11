f9cd811df5d37e562584151cdf12b68dd0126b9a
Matéo Mévollon <m.mevollon@protonmail.com>
2022-04-11 11:50:02 +0200

    test: fix test

--- __tests__/services/fetchers/nextcloud.test.js
+++ __tests__/services/fetchers/nextcloud.test.js
@@ -17,15 +17,16 @@ it("should return a list of fetched users", async () => {
     )
     .mockResolvedValue(
       Promise.resolve({
-        json: () => Promise.resolve({ ocs: { data: { login: "login" } } }),
+        json: () =>
+          Promise.resolve({ ocs: { data: { login: "login", enabled: true } } }),
       })
     )
 
   const result = await fetchNextcloudUsers()
   expect(result).toStrictEqual([
-    { login: "login" },
-    { login: "login" },
-    { login: "login" },
+    { login: "login", enabled: true },
+    { login: "login", enabled: true },
+    { login: "login", enabled: true },
   ])
 })
 
@@ -34,22 +35,4 @@ it("should return an empty list if no response", async () => {
 
   const result = await fetchNextcloudUsers()
   expect(result).toStrictEqual([])
-})
-
-it("should return empty users if no response for users query", async () => {
-  fetcher
-    .mockResolvedValueOnce(
-      Promise.resolve({
-        json: () => Promise.resolve({ ocs: { data: { users: ["", "", ""] } } }),
-      })
-    )
-    .mockResolvedValueOnce(
-      Promise.resolve({
-        json: () => Promise.resolve({ ocs: { data: { users: [] } } }),
-      })
-    )
-    .mockResolvedValue(Promise.resolve(null))
-
-  const result = await fetchNextcloudUsers()
-  expect(result).toStrictEqual([{}, {}, {}])
 })

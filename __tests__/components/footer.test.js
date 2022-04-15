f74a04c8cce00ed70766758dbcaee461457fa86e
gary-van-woerkens <gary.van-woerkens@sg.social.gouv.fr>
2022-04-15 14:51:51 +0200

    update tests

--- __tests__/components/footer.test.js
+++ __tests__/components/footer.test.js
@@ -2,6 +2,20 @@ import { render } from "@testing-library/react"
 
 import Footer from "../../src/components/footer"
 
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+})
+
 process.env.NEXT_PUBLIC_APP_VERSION = "0.0.42"
 process.env.NEXT_PUBLIC_APP_VERSION_COMMIT =
   "2f7c23024eeb820ed67aef00bc40e7955111cbf4"

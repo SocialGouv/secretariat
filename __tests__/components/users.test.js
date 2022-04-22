2e1464bb3d89e60fa6de56be98081d55c5a9bc03
gary-van-woerkens <gary.van-woerkens@sg.social.gouv.fr>
2022-04-22 16:01:27 +0200

    decrease timeout, again

--- __tests__/components/users.test.js
+++ __tests__/components/users.test.js
@@ -19,7 +19,7 @@ describe("when users are loaded", () => {
     container = rendered.container
     await waitForElementToBeRemoved(
       await screen.getByText("Aucun utilisateur sélectionné."),
-      { timeout: 3000 }
+      { timeout: 10000 }
     )
   })
 

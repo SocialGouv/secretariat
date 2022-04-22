8926fce17668f954df93248ae489269603dc9dac
gary-van-woerkens <gary.van-woerkens@sg.social.gouv.fr>
2022-04-22 20:51:23 +0200

    add env variable

--- jest.setup.js
+++ jest.setup.js
@@ -3,6 +3,8 @@ import "whatwg-fetch"
 
 import { server } from "./src/mocks/server"
 
+process.env.NEXT_PUBLIC_HASURA_URL = "http://localhost:8080/v1/graphql"
+
 beforeAll(() => {
   server.listen()
 })

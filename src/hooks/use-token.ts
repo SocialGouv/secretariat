3ac2400cb4fb522b0e8a8635e027bb91313cf8b4
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2022-04-25 17:32:22 +0200

    fix: remove reference to localhost:3000 (#239)

--- src/hooks/use-token.ts
+++ src/hooks/use-token.ts
@@ -1,8 +1,8 @@
 import useSWRImmutable from "swr/immutable"
 
-const fetcher = (url: string) => {
+const fetcher = async (url: string) => {
   // Tests run in node environment where only absolute urls are supported
-  url = /$http/.test(url) ? url : `http://localhost:3000${url}`
+  url = !!window.location.port ? url : `http://localhost:3000${url}`
   return fetch(url).then((r) => r.json())
 }
 

59a298b7c25a2beeea7f7bb9bd108494c305ec75
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2022-04-26 17:06:04 +0200

    fix: alerts style (#246)

--- src/components/common/alert.tsx
+++ src/components/common/alert.tsx
@@ -1,10 +1,12 @@
 const Alert = ({ message, type }: { message: string; type: string }) => (
   <div className={`alert ${type}`}>
-    {type === "info" && <i className="ri-information-fill"></i>}
-    {type === "success" && <i className="ri-checkbox-circle-fill"></i>}
-    {type === "warning" && <i className="ri-alert-fill"></i>}
-    {type === "error" && <i className="ri-close-circle-fill"></i>}
-    {message}
+    <div className="icon">
+      {type === "info" && <i className="ri-information-fill"></i>}
+      {type === "success" && <i className="ri-checkbox-circle-fill"></i>}
+      {type === "warning" && <i className="ri-alert-fill"></i>}
+      {type === "error" && <i className="ri-close-circle-fill"></i>}
+    </div>
+    <div className="message">{message}</div>
   </div>
 )
 

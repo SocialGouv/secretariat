7dc8e93a500eca0b50b8b9e98b1921c0d98c5c83
Matéo Mévollon <m.mevollon@protonmail.com>
2022-05-03 14:22:24 +0200

    fix: code smell

--- src/pages/api/sync.ts
+++ src/pages/api/sync.ts
@@ -1,7 +1,7 @@
 import sync from "@/services/sync"
 import type { NextApiRequest, NextApiResponse } from "next"
 
-const Sync = async (req: NextApiRequest, res: NextApiResponse) => {
+const Sync = async (_req: NextApiRequest, res: NextApiResponse) => {
   sync()
   res.status(202).end()
 }

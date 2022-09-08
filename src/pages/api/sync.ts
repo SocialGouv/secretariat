76d07ac0a5fcbaf2028f0539a157aa1aa9e3b163
Matéo Mévollon <m.mevollon@protonmail.com>
2022-09-08 15:30:47 +0200

    sync action

--- src/pages/api/sync.ts
+++ src/pages/api/sync.ts
@@ -1,7 +1,10 @@
 import sync from "@/services/sync"
+import { getJwt } from "@/utils/jwt"
+import logAction from "@/utils/log-action"
 import type { NextApiRequest, NextApiResponse } from "next"
 
 const Sync = async (_req: NextApiRequest, res: NextApiResponse) => {
+  logAction({ action: "sync", token: getJwt() })
   sync()
   res.status(200).json({ message: "sync started" })
 }

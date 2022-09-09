25c8070bf40e82dab0d92a0e77f1e6c970e2c41d
Matéo Mévollon <m.mevollon@protonmail.com>
2022-09-09 11:41:59 +0200

    graphQLFetcher comments

--- src/utils/graphql-fetcher.ts
+++ src/utils/graphql-fetcher.ts
@@ -1,6 +1,8 @@
 import { GraphQLClient } from "graphql-request"
 import { NEXT_PUBLIC_HASURA_URL } from "./env"
 
+// Used for all GraphQL interactions with Hasura
+// These can be the client and the Next.js backend
 const graphQLFetcher = ({
   query,
   includeCookie,
@@ -9,8 +11,10 @@ const graphQLFetcher = ({
 }: GraphQLFetcherParams) => {
   let options = {}
   if (includeCookie) {
+    // typically used by client : forward the JWT session cookie to backend
     options = { ...options, credentials: "include" }
   } else if (token) {
+    // typically used by backend : include a manually forged JWT
     options = {
       ...options,
       credentials: "omit",

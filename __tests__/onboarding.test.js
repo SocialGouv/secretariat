205693a24f1a8044fd5aab541533d2bd855f2787
Matéo Mévollon <m.mevollon@protonmail.com>
2022-09-12 18:18:50 +0200

    test: mock Date object

--- __tests__/onboarding.test.js
+++ __tests__/onboarding.test.js
@@ -1,7 +1,4 @@
 import { render } from "@testing-library/react"
-import { useRouter } from "next/router"
-
-import Onboarding from "../src/pages/onboarding"
 
 jest.mock("next/router", () => ({
   useRouter() {
@@ -14,7 +11,18 @@ jest.mock("next/router", () => ({
   },
 }))
 
+beforeAll(() => {
+  jest.useFakeTimers()
+  jest.setSystemTime(new Date("20 Aug 2020 00:12:00 GMT").getTime())
+})
+
+afterAll(() => {
+  jest.useRealTimers()
+})
+
 it("renders onboarding page", () => {
+  // Import has to happen after the jest setup that mocks the Date object
+  const Onboarding = require("../src/pages/onboarding").default
   const { container } = render(<Onboarding />)
   expect(container).toMatchSnapshot()
 })

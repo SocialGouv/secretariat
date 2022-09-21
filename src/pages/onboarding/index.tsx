8e1b3b307de1b4481d7398cb4c2cd42ee5fa2cc8
gary-van-woerkens <gary.van-woerkens@sg.social.gouv.fr>
2022-09-22 00:48:00 +0200

    fix: layout behaviour on scroll

--- src/pages/onboarding/index.tsx
+++ src/pages/onboarding/index.tsx
@@ -1,21 +1,16 @@
 import Head from "next/head"
-import { useSession } from "next-auth/react"
 
 import OnboardingForm from "@/components/onboarding-form"
 import OnboardingHeader from "@/components/onboarding-header"
 
-const Onboarding = () => {
-  const { data: session } = useSession()
-
-  return (
-    <main className={session ? "mt-12" : ""}>
-      <Head>
-        <title>Secrétariat</title>
-      </Head>
-      <OnboardingHeader />
-      <OnboardingForm />
-    </main>
-  )
-}
+const Onboarding = () => (
+  <main>
+    <Head>
+      <title>Secrétariat</title>
+    </Head>
+    <OnboardingHeader />
+    <OnboardingForm />
+  </main>
+)
 
 export default Onboarding

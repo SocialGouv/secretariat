10655555535e4f6d61d5bda71e6afeb06cf827d7
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2022-10-18 11:11:51 +0200

    fix(onboarding-request): show review info instead of validation button (#424)

--- src/components/onboarding-form/form.tsx
+++ src/components/onboarding-form/form.tsx
@@ -1,12 +1,16 @@
+import Alert from "@/components/common/alert"
+
 import UserInfo from "./user-info"
 import GithubServices from "./github-services"
 import DefaultServices from "./default-services"
 
 const Form = ({
   status,
+  review,
   onSubmit,
 }: {
-  status: "create" | "review"
+  review?: { author: string; date: string }
+  status: "create" | "review" | "reviewed"
   onSubmit: () => void
 }) => (
   <form
@@ -19,9 +23,19 @@ const Form = ({
     <DefaultServices />
     <GithubServices />
 
-    <button className="primary" type="submit">
-      {status === "create" ? "Envoyer" : "Valider"}
-    </button>
+    {status === "reviewed" ? (
+      <Alert
+        type="info"
+        title="Demande validée"
+        message={`Cette demande d'embarquement a été validée${
+          review?.author && ` par ${review.author} le ${review.date}`
+        }.`}
+      />
+    ) : (
+      <button className="primary" type="submit">
+        {status === "create" ? "Envoyer" : "Valider"}
+      </button>
+    )}
   </form>
 )
 

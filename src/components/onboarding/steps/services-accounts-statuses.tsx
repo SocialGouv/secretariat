5358974cd106d92edb30dc7bc56947d268fd4398
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2023-01-16 16:22:53 +0100

    feat: improve onboarding result page (#468)

--- src/components/onboarding/steps/services-accounts-statuses.tsx
+++ src/components/onboarding/steps/services-accounts-statuses.tsx
@@ -1,31 +1,47 @@
 import statusOk from "@/utils/status-ok"
-import Alert from "@/components/common/alert"
 
 type ServicesAccountsStatuses = Record<
   ServiceName,
   { status: number; body: string | Record<"message", string> }
 >
 
+const services: Record<string, Record<"icon" | "message", string>> = {
+  github: {
+    icon: "ri-git-pull-request-line",
+    message: "Obtenir un accès aux repositories de SocialGouv (Github)",
+  },
+  mattermost: {
+    icon: "ri-wechat-line",
+    message: "Obtenir un compte de messagerie instantanée (Mattermost)",
+  },
+}
+
 const ServicesAccountsStatuses = ({
   statuses,
 }: {
   statuses?: ServicesAccountsStatuses
 }) => (
-  <div className="servicesAccountsStatuses">
-    {statuses &&
-      Object.entries(statuses).map(([service, status], i) => (
-        <Alert
-          key={i}
-          title={service}
-          type={statusOk(status.status) ? "success" : "error"}
-        >
-          <>
-            {typeof status.body === "string"
-              ? status.body
-              : status.body.message}
-          </>
-        </Alert>
-      ))}
+  <div className="services-accounts-statuses">
+    <ul className="services-list">
+      {statuses &&
+        Object.entries(statuses).map(([service, { body, status }], i) => (
+          <li key={i} className={`service selected`}>
+            <i className={`${services[service].icon} icon`} />
+            <div className="flex-1">
+              <p>{services[service].message}</p>
+              <pre className="text-sm rounded mt-2 py-2 px-3 bg-red-marianne-950-main text-red-marianne-425-main">
+                {typeof body === "string" ? body : body.message}
+              </pre>
+            </div>
+
+            {statusOk(status) ? (
+              <i className="ri-check-line check" />
+            ) : (
+              <i className="ri-close-line check error" />
+            )}
+          </li>
+        ))}
+    </ul>
   </div>
 )
 

690aae493bffb37d353c428400ad0a07ba5a37d6
Matéo Mévollon <m.mevollon@proton.me>
2023-02-15 15:46:16 +0100

    fix: record zammad id as string

--- __tests__/services/fetchers/zammad.test.js
+++ __tests__/services/fetchers/zammad.test.js
@@ -6,12 +6,18 @@ jest.mock("@/utils/rest-fetcher")
 it("should return a list of fetched users", async () => {
   fetcher
     .mockResolvedValueOnce(
-      Promise.resolve({ json: () => Promise.resolve([{}, {}, {}]) })
+      Promise.resolve({
+        json: () => Promise.resolve([{ id: 0 }, { id: 1 }, { id: 2 }]),
+      })
     )
     .mockResolvedValue(Promise.resolve({ json: () => Promise.resolve([]) }))
 
   const result = await fetchZammadUsers()
-  expect(result).toStrictEqual([{ groups: [] }, { groups: [] }, { groups: [] }])
+  expect(result).toStrictEqual([
+    { id: "0", groups: [] },
+    { id: "1", groups: [] },
+    { id: "2", groups: [] },
+  ])
 })
 
 it("should return an empty list if no response", async () => {

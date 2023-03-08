5f93c9a98cd238872ac3f42d21ebb09ee19a04e1
Matéo Mévollon <m.mevollon@proton.me>
2023-03-08 15:07:22 +0100

    fix: missing logger instead of console.log

--- src/utils/rest-fetcher.ts
+++ src/utils/rest-fetcher.ts
@@ -1,7 +1,9 @@
+import logger from "./logger"
+
 const fetcher = async (url: string, params: Record<string, unknown>) => {
   const response = await fetch(url, params)
   if (!response.ok) {
-    console.error("Error status in response", response)
+    logger.error({ response }, "Error status in response")
     return null
   }
   return response

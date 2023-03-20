89d007b011cd1ea994e32048e687adde1f4656ab
Matéo Mévollon <m.mevollon@proton.me>
2023-03-20 11:50:46 +0100

    fix: upgrade Next to upgrade NextAuth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react"
 import type { AppProps } from "next/app"
 import { init } from "@socialgouv/matomo-next"
 import { SessionProvider } from "next-auth/react"
+import { Session } from "next-auth"
 
 import Layout from "@/components/layout"
 
@@ -11,7 +12,10 @@ import "@/styles/globals.scss"
 const MATOMO_URL = process.env.NEXT_PUBLIC_MATOMO_URL || ""
 const MATOMO_SITE_ID = process.env.NEXT_PUBLIC_MATOMO_SITE_ID || ""
 
-function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps<{ session: Session }>) {
   useEffect(() => {
     init({ url: MATOMO_URL, siteId: MATOMO_SITE_ID })
   }, [])

7cb07f29c8b5dd5116b1494fdbfdf40885344f5b
Matéo Mévollon <m.mevollon@proton.me>
2023-10-20 16:26:45 +0200

    dead code comment

--- src/pages/api/revoke.ts
+++ src/pages/api/revoke.ts
@@ -13,32 +13,32 @@ const Revoke = async (req: NextApiRequest, res: NextApiResponse) => {
   res.status(403).json({ message: "Endpoint Disabled" })
   return
 
-  if (req.method !== "DELETE") {
-    res.setHeader("Allow", "DELETE")
-    res.status(405).json({ message: "Method Not Allowed" })
-    return
-  }
-
-  const userToken = await getToken({ req, decode, cookieName: COOKIE_NAME })
-
-  if (!userToken) {
-    res.status(403).json({ message: "Unauthorized" })
-    return
-  }
-
-  logAction({
-    action: "revoke",
-    user: userToken.user.login,
-    token: getJwt(),
-    parameters: JSON.stringify(req.body.input.data),
-  })
-
-  const { accountServiceID, accountID, serviceName } = req.body.input.data
-  if (SERVICES.includes(serviceName)) {
-    res.status(200).json(await revoke(accountServiceID, accountID, serviceName))
-  } else {
-    res.status(400).json({ message: "unknown service name" })
-  }
+  // if (req.method !== "DELETE") {
+  //   res.setHeader("Allow", "DELETE")
+  //   res.status(405).json({ message: "Method Not Allowed" })
+  //   return
+  // }
+  //
+  // const userToken = await getToken({ req, decode, cookieName: COOKIE_NAME })
+  //
+  // if (!userToken) {
+  //   res.status(403).json({ message: "Unauthorized" })
+  //   return
+  // }
+  //
+  // logAction({
+  //   action: "revoke",
+  //   user: userToken.user.login,
+  //   token: getJwt(),
+  //   parameters: JSON.stringify(req.body.input.data),
+  // })
+  //
+  // const { accountServiceID, accountID, serviceName } = req.body.input.data
+  // if (SERVICES.includes(serviceName)) {
+  //   res.status(200).json(await revoke(accountServiceID, accountID, serviceName))
+  // } else {
+  //   res.status(400).json({ message: "unknown service name" })
+  // }
 }
 
 export default Revoke

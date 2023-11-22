9cb5a43367d89bcc44ac1a4bdb36f3f4d9f9d937
Matéo Mévollon <m.mevollon@proton.me>
2023-11-22 18:41:14 +0100

    release

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -4,4 +4,5 @@ Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
   environment: process.env.NEXT_PUBLIC_ENVIRONMENT,
   tracesSampleRate: 1.0,
+  release: process.env.NEXT_PUBLIC_RELEASE,
 })

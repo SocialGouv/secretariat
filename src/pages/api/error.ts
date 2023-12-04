089942c057ce8f8ff5a0456058b57a64c12fc059
Matéo Mévollon <m.mevollon@proton.me>
2023-12-04 12:10:55 +0100

    remove test errors

--- src/pages/api/error.ts
+++
@@ -1,8 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next"
-
-const Report = (req: NextApiRequest, res: NextApiResponse) => {
-  throw new Error("serve error")
-  res.status(200).end()
-}
-
-export default Report

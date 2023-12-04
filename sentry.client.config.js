7df32a57c2d67706e73d2522ce863d8b59818d43
Matéo Mévollon <m.mevollon@proton.me>
2023-12-04 10:42:09 +0100

    test env var

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -1,8 +1,8 @@
 import * as Sentry from "@sentry/nextjs"
 
 Sentry.init({
-  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-  environment: process.env.NEXT_PUBLIC_ENVIRONMENT,
-  tracesSampleRate: 1.0,
+  // dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  // environment: process.env.NEXT_PUBLIC_ENVIRONMENT,
+  // tracesSampleRate: 1.0,
   // release: process.env.NEXT_PUBLIC_RELEASE,
 })

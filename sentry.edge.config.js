88b8d9bf4ac568bbb158b9763a228aae2c896943
Matéo Mévollon <m.mevollon@proton.me>
2023-12-04 11:00:06 +0100

    test

--- sentry.edge.config.js
+++ sentry.edge.config.js
@@ -1,8 +1,3 @@
 import * as Sentry from "@sentry/nextjs"
 
-Sentry.init({
-  // dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-  // environment: process.env.NEXT_PUBLIC_ENVIRONMENT,
-  // tracesSampleRate: 1.0,
-  // release: process.env.NEXT_PUBLIC_RELEASE,
-})
+Sentry.init({})

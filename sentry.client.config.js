2428c090389f8d16e1d6c96e5c235ced072d1ca3
Matéo Mévollon <m.mevollon@proton.me>
2023-12-04 11:36:33 +0100

    test

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -1,3 +1,7 @@
 import * as Sentry from "@sentry/nextjs"
 
-Sentry.init({})
+Sentry.init({
+  release: process.env.NEXT_PUBLIC_SENTRY_RELEASE,
+  environment: process.env.NEXT_PUBLIC_SENTRY_ENVIRONMENT,
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+})

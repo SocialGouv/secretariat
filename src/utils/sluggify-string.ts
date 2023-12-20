b3793937ed2c9ccb3911095bde4b44cb4510f071
Matéo Mévollon <m.mevollon@proton.me>
2023-12-20 10:42:01 +0100

    fix: sanitize quote in user's name

--- src/utils/sluggify-string.ts
+++ src/utils/sluggify-string.ts
@@ -3,6 +3,7 @@ const sluggifyString = (input: string) =>
     .normalize("NFKD") // break down characters and diacritics
     .replace(/\p{Diacritic}/gu, "") // remove diacritics
     .replace(/\s+/g, "-") // remove whitespaces
+    .replace(/'+/g, "")
     .toLowerCase()
 
 export default sluggifyString

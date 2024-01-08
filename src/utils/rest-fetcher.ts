d0efce8ee281bf07c60110588ea4e668571a8592
Matéo Mévollon <m.mevollon@proton.me>
2024-01-08 09:07:56 +0100

    fix: try/catch around fetch

--- src/utils/rest-fetcher.ts
+++ src/utils/rest-fetcher.ts
@@ -1,12 +1,16 @@
 import logger from "./logger"
 
 const fetcher = async (url: string, params: Record<string, unknown>) => {
-  const response = await fetch(url, params)
-  if (!response.ok) {
-    logger.error({ response }, "Error status in response")
-    return null
+  try {
+    const response = await fetch(url, params)
+    if (!response.ok) {
+      logger.error({ response }, "Error status in response")
+      return null
+    }
+    return response
+  } catch (error) {
+    logger.error("Error attempting to fetch", error)
   }
-  return response
 }
 
 export default fetcher

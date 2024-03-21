9fdf4f9ba7ae9806bde3b656d7e35552b73bb0bd
Matéo Mévollon <m.mevollon@proton.me>
2024-03-21 14:12:35 +0100

    log report

--- src/pages/api/report.ts
+++ src/pages/api/report.ts
@@ -1,8 +1,10 @@
 import httpLogger from "@/utils/http-logger"
+import logger from "@/utils/logger"
 import { NextApiRequest, NextApiResponse } from "next"
 
 const Report = (req: NextApiRequest, res: NextApiResponse) => {
   httpLogger(req, res)
+  logger.info({ body: req.body })
   res.status(200).end()
 }
 
